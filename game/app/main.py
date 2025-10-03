d64d521c3370f2721562e7ebb0d06f5fdc10a840
osudod <gitaddhub@gmail.com>
2025-10-03 21:09:13 +0800

    Добавил музыку. Понял что нужно переделывать структуру

--- game/app/main.py
+++ game/app/main.py
@@ -1,12 +1,13 @@
 
 import os
 import sys
+import math
 
 if not __package__:
-    __package__ = (
-        (lambda p, n:
-        (".".join(p[-n:]), sys.path.insert(0, os.sep.join(p[:-n])))[0])(
-            os.path.realpath(__file__).split(os.sep)[:-1], 2))
+    __package__ = ((lambda p, d:
+                    (".".join(p[-(n := p[::-1].index(d) + 1):]),
+                    sys.path.insert(0, os.sep.join(p[:-n])))[0])(
+                        os.path.realpath(__file__).split(os.sep)[:-1], "game"))
 
 import pygame
 import level1
@@ -23,6 +24,13 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Dead world")
 clock = pygame.time.Clock()
 
+musc = pygame.mixer.music.load("game/assets/music/keys-of-moon-lonesome-journey.mp3")
+music_volume = 1
+pygame.mixer.music.set_volume(music_volume)
+pygame.mixer.music.play(-1)
+
+
+
 font_large = pygame.font.SysFont('Arial', 64)
 font_small = pygame.font.SysFont('Arial', 32)
 
@@ -32,6 +40,16 @@ settings_button = Button(250, 300, 300, 60, "Настройки")
 exit_button = Button(250, 400, 300, 60, "Выход")
 
 back_button_settings = Button(40,75,100,60,"Назад")
+
+def slider(sc, coor, len_, units, active_unit, mcoor, LKM):
+    global peremennaya
+    xm, ym = mcoor[0], mcoor[1]
+    pygame.draw.line(sc, 'black', coor, (coor[0]+len_, coor[1]), 3)
+    step = len_//units
+    pygame.draw.circle(sc, 'red', (coor[0]+step*active_unit, coor[1]), 10)
+    if LKM and math.hypot(xm - (coor[0]+step*active_unit), ym - coor[1]) <= 20:
+        if (xm - coor[0])//step >= 1 and (xm - coor[0])//step <= units:
+            peremennaya = (xm - coor[0])//step
 
 def draw_menu():
     screen.fill(THEME) 
@@ -47,15 +65,22 @@ def draw_menu():
     
     pygame.display.flip()
 
-def draw_settings():
+def draw_settings(xm, ym, LKM):
     screen.fill("#984141")
     
     title = font_large.render("Настройки", True, "#ffffff")
     title_rect = title.get_rect(center=(SCREEN_WIDTH//2, 100))
     
+    sound_set = font_small.render("Музыка", True, "#ffffff")
+    sound_set_rect = sound_set.get_rect(center=(160, 200))
+    
     back_button_settings.draw(screen=screen, font=font_small)
     
     screen.blit(title, title_rect)
+    
+    screen.blit(sound_set, sound_set_rect)
+    
+    slider(screen, [250,200], 200, 10, peremennaya, [xm,ym], LKM)
     
     pygame.display.flip()
 
@@ -70,9 +95,9 @@ def handle_menu_click(pos):
         return "назад_настройки"
     
     return None
-
 game_state = 'menu'
-
+LKM = False
+peremennaya = 0
 running = True
 while running:
     for event in pygame.event.get():
@@ -88,12 +113,19 @@ while running:
                 game_state = "settings"
             elif clicked_option == "назад_настройки":
                 game_state = "menu"
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                LKM = True
+        if event.type == pygame.MOUSEBUTTONUP:
+            if event.button == 1:
+                LKM = False
     if game_state == "menu":
         draw_menu()
     elif game_state == "playing":
         level1.play(screen)
     elif game_state == "settings":
-        draw_settings()
+        xm, ym = pygame.mouse.get_pos()
+        draw_settings(xm, ym, LKM)
     clock.tick(FPS)
 
 pygame.quit()
